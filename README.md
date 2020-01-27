31c9c710369df0f26346634695159b33f90eb4f2
Adriana Luchian <adriana.dumitras@gmail.com>
2020-01-27 11:14:02 +0100

    Update project
    - add cors
    - update README to offer more info
    - change exposed port to 80
    - update docker ignore file to exclude not needed content

--- README.md
+++ README.md
@@ -2,10 +2,23 @@
 
 Basic NodeJS API.
 
-## npm scripts
+## Development setup
 
-`lint`: runs linting rules
+`npm install`: installs project dependencies
 
-`test`: runs all tests found in the 'src' folder
+`npm run lint`: runs linting rules
 
-`start`: run server locally
+`npm test`: runs tests
+
+`npm run start`: runs server locally
+
+## Running the API in a Docker container
+
+`docker build . -api`
+
+`docker run -p -d 8001:80 api`
+
+The API will be accessible at http://localhost:8001
+
+To stop the container:
+`docker stop api`
--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
+const cors = require('cors');
 const express = require('express');
 const graceful = require('node-graceful');
 
-const port = 3000;
+const port = 80;
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello'));
 
 const server = app.listen(port, () => console.log(`Server listening on port ${port}`));
 
 graceful.on('exit', () => {
-    return new Promise((resolve, reject) => {
-      server.close((error) => {
-        if (error) {
-            return reject(error);
-        }
-        resolve();
-      });
+  return new Promise((resolve, reject) => {
+    server.close((error) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve();
     });
+  });
 });
